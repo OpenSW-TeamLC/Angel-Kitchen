7cb62e810235502f46a7c2de40eb19c20bfbadd8
Leesungsup <tmdtjq0116@naver.com>
2021-08-04 02:34:18 +0900

    naver map (marker)

--- app/src/main/java/hello/world/angelkitchen/MainActivity.kt
+++ app/src/main/java/hello/world/angelkitchen/MainActivity.kt
@@ -1,11 +1,65 @@
 package hello.world.angelkitchen
 
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import androidx.annotation.UiThread
+import androidx.fragment.app.FragmentActivity
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.geometry.LatLngBounds
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.InfoWindow
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.Overlay
+import com.naver.maps.map.overlay.OverlayImage
+import com.naver.maps.map.util.FusedLocationSource
+import com.naver.maps.map.widget.ZoomControlView
+import java.util.concurrent.Executor
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map, it).commit()
+            }
+        mapFragment.getMapAsync(this)
     }
-}
+
+    override fun onMapReady(naverMap: NaverMap) {
+        val cameraPosition = CameraPosition(
+            LatLng(37.5666102, 126.9783881), // 대상 지점
+            16.0, // 줌 레벨
+            20.0, // 기울임 각도
+            180.0 // 베어링 각도
+        )
+
+        Toast.makeText(this,
+            "대상 지점 위도: ${cameraPosition.target.latitude}, " +
+                    "대상 지점 경도: ${cameraPosition.target.longitude}, " +
+                    "줌 레벨: ${cameraPosition.zoom}, " +
+                    "기울임 각도: ${cameraPosition.tilt}, " +
+                    "베어링 각도: ${cameraPosition.bearing}",
+            Toast.LENGTH_SHORT).show()
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.5666102, 126.9783881))
+        naverMap.moveCamera(cameraUpdate)
+        val uiSettings = naverMap.uiSettings
+        uiSettings.isCompassEnabled=true
+        val marker = Marker()
+        marker.position = LatLng(37.5670135, 126.9783740)
+        marker.map = naverMap
+        val infoWindow = InfoWindow()
+        infoWindow.adapter = object : InfoWindow.DefaultTextAdapter(this) {
+            override fun getText(infoWindow: InfoWindow): CharSequence {
+                return "정보 창 내용"
+            }
+        }
+        }
+    }
+
