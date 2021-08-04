adba9183f4abcfab2454ef9dfd004529c7104b57
Leesungsup <tmdtjq0116@naver.com>
2021-08-05 01:46:46 +0900

    naver map (marker 가시거리별 생성조절)

--- app/src/main/java/hello/world/angelkitchen/MainActivity.kt
+++ app/src/main/java/hello/world/angelkitchen/MainActivity.kt
@@ -18,7 +18,13 @@ import com.naver.maps.map.overlay.Overlay
 import com.naver.maps.map.overlay.OverlayImage
 import com.naver.maps.map.util.FusedLocationSource
 import com.naver.maps.map.widget.ZoomControlView
+import java.util.*
 import java.util.concurrent.Executor
+import com.naver.maps.map.CameraPosition
+
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.NaverMap.OnCameraChangeListener
+
 
 class MainActivity : FragmentActivity(), OnMapReadyCallback {
     private val LOCATION_PERMISSION_REQUEST_CODE = 1000
@@ -46,11 +52,11 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
         val marker3 = Marker()
 
         marker1.position = LatLng(37.5770135, 126.9783740)
-        marker1.map = naverMap
+        //marker1.map = naverMap
         marker2.position = LatLng(37.5670135, 126.9783740)
-        marker2.map = naverMap
+        //marker2.map = naverMap
         marker3.position = LatLng(37.5680135, 126.9783740)
-        marker3.map = naverMap
+        //marker3.map = naverMap
 
         naverMap.setOnMapClickListener { coord, point -> infoWindow.close() }
 
@@ -67,14 +73,54 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
             infoWindow.open(marker2)
             true
         }
-
+        val markers= listOf(marker1.position,marker2.position,marker3.position)
         infoWindow.adapter = object : InfoWindow.DefaultTextAdapter(this) {
             override fun getText(infoWindow: InfoWindow): CharSequence {
                 // 정보 창이 열린 마커의 tag를 텍스트로 노출하도록 반환
                 return infoWindow.marker?.tag as CharSequence? ?: ""
             }
         }
+        naverMap.addOnCameraChangeListener { reason, animated ->
+            freeActiveMarkers()
+            // 정의된 마커위치들중 가시거리 내에있는것들만 마커 생성
+            val currentPosition = getCurrentPosition(naverMap)
+            for (markerPosition in markers) {
+                if (!withinSightMarker(LatLng(38.5680135, 126.9783740), markerPosition)) continue
+                val marker = Marker()
+                marker.position = markerPosition
+                marker.map = naverMap
+            }
+        }
 
+    }
+    private var markersPosition: Vector<LatLng>? = null
+    private var activeMarkers: Vector<Marker>? = null
+    fun getCurrentPosition(naverMap: NaverMap): LatLng? {
+        val cameraPosition = naverMap.cameraPosition
+        return LatLng(cameraPosition.target.latitude, cameraPosition.target.longitude)
+    }
+
+    // 선택한 마커의 위치가 가시거리(카메라가 보고있는 위치 반경 3km 내)에 있는지 확인
+    val REFERANCE_LAT = 1 / 109.958489129649955
+    val REFERANCE_LNG = 1 / 88.74
+    val REFERANCE_LAT_X3 = 3 / 109.958489129649955
+    val REFERANCE_LNG_X3 = 3 / 88.74
+    fun withinSightMarker(currentPosition: LatLng, markerPosition: LatLng): Boolean {
+        val withinSightMarkerLat =
+            Math.abs(currentPosition.latitude - markerPosition.latitude) <= REFERANCE_LAT_X3
+        val withinSightMarkerLng =
+            Math.abs(currentPosition.longitude - markerPosition.longitude) <= REFERANCE_LNG_X3
+        return withinSightMarkerLat && withinSightMarkerLng
+    }
+    private fun freeActiveMarkers() {
+        if (activeMarkers == null) {
+            activeMarkers = Vector()
+            return
+        }
+        for (activeMarker in activeMarkers!!) {
+            activeMarker.map = null
+        }
+        activeMarkers = Vector()
     }
 }
 
