901423fb24058730d70a36ad00550165eb9eed71
HanBI24 <alsltjdrhs@naver.com>
2021-08-08 13:13:43 +0900

    Set Navigation in JetPack

---
+++ app/src/main/java/hello/world/angelkitchen/view/onboard/ThirdFragment.kt
@@ -0,0 +1,10 @@
+package hello.world.angelkitchen.view.onboard
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class ThirdFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
